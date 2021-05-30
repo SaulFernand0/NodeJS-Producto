ce4e3fc1c3caef94d252cff528a6199c54cf59cb
SaulFernand0 <saulolivas@upeu.edu.pe>
2021-05-30 14:48:12 -0500

    v3.0

--- src/app.js
+++ src/app.js
@@ -8,11 +8,9 @@ var cors = require('cors');
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
-
 app.get('/', function(req, res, next){
     res.send('Bienvenido a Node JS...!');
 });
-
 app.use('/api/auth/users', userRoutes);
 
 module.exports = app;
