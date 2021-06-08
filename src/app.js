5b20ac8c5de37ed80143fb5007b3c530732d53ad
SaulFernand0 <saulolivas@upeu.edu.pe>
2021-06-08 01:05:15 -0500

    v1.0

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 const app = express();
 var cors = require('cors');
@@ -12,5 +13,6 @@ app.get('/', function(req, res, next){
     res.send('Bienvenido a Node JS...!');
 });
 app.use('/api/auth/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,10 +4,12 @@ const router = Router();
 
 import * as userCtr from '../controllers/user.controller'
 
-router.get('/', userCtr.readAllUsers);
-router.get('/:id', userCtr.readUsers);
-router.post('/add', userCtr.createUser);
-router.put('/update/:id', userCtr.updateUsers);
-router.delete('/delete/:id', userCtr.delUsers);
+const { checkToken } = require('../auth/token_validation');
+
+router.get('/', checkToken, userCtr.readAllUsers);
+router.get('/:id', checkToken, userCtr.readUser);
+router.post('/add', checkToken, userCtr.createUser);
+router.put('/update/:id', checkToken, userCtr.updateUser);
+router.delete('/delete/:id', checkToken, userCtr.dellUser);
 
 export default router;
